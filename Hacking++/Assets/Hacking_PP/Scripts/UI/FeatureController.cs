4bb84fbc1427547fa83b59774caf29ceecd9e0db
Andrew Castillo <valax321@gmail.com>
2018-04-21 13:20:50 +1000

    Work

---
+++ Hacking++/Assets/Hacking_PP/Scripts/UI/FeatureController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureController : MonoBehaviour
+{
+    public enum Feature
+    {
+        HackerManual
+    }
+
+    [Header("Feature: Hacker Manual")]
+    [SerializeField] private GameObject hackerManual;
+
+    private void Awake()
+    {
+        hackerManual.SetActive(false);
+    }
+
+    public void UnlockFeature(Feature f)
+    {
+        switch (f)
+        {
+            case Feature.HackerManual:
+                hackerManual.SetActive(true);
+                break;
+        }
+    }
+}
--- Hacking++/Assets/Hacking_PP/Scripts/UI/TerminalController.cs
+++ Hacking++/Assets/Hacking_PP/Scripts/UI/TerminalController.cs
@@ -1,20 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TerminalController : MonoBehaviour
 {
-    Queue<string> messages = new Queue<string>(1024);
+    const int MAX_BUFFER_COUNT = 1024;
+    Queue<string> messages = new Queue<string>();
 
-    // Use this for initialization
-    void Start()
+    const string errorColour = "#ca861b";
+
+    Text text;
+
+    private void Awake()
     {
-        
+        text = GetComponent<Text>();
+        text.text = string.Empty;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Print(string msg)
     {
+        messages.Enqueue(msg);
+        if (messages.Count > MAX_BUFFER_COUNT)
+        {
+            string rem = messages.Dequeue();
+            text.text.Remove(0, rem.Length + 1); //+1 because of \n
+        }
 
+        text.text += "\n" + msg;
+    }
+
+    public void Error(string msg)
+    {
+        msg = string.Format("<color={0}>{1}</color>", errorColour, msg);
+        messages.Enqueue(msg);
+        if (messages.Count > MAX_BUFFER_COUNT)
+        {
+            string rem = messages.Dequeue();
+            text.text.Remove(0, rem.Length + 1); //+1 because of \n
+        }
+
+        text.text += "\n" + msg;
     }
 }
